dcb95ae85d97fd3e750fd54cefe412b7a0aeeeb6
TaThuHa <thuhak2tltnpt@gmail.com>
2023-03-02 04:35:51 +0700

    up

---
+++ src/rikkei/academy/Main.java
@@ -0,0 +1,10 @@
+package rikkei.academy;
+
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println("time out "+date);
+    }
+}
